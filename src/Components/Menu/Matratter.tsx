da44257baf8837590e2bdaf2a59f9b1545981391
Ali Al-Nasir <iraki-ali17@hotmail.com>
2023-11-07 22:15:51 +0100

    Hämtar data från databas

--- src/Components/Menu/Matratter.tsx
+++ src/Components/Menu/Matratter.tsx
@@ -1,43 +1,56 @@
-
-import '../assets/Matratter.css'; 
+import { useEffect, useState } from "react";
+import "../assets/Matratter.css";
 const Matratter = () => {
-  const matratterData = [
-    {
-        id: 1,
-        name: 'Thai tacos ',
-        description: 'Ta klassiska thailändska smaker som pad thai, gröna curry och satay och servera dem som fyllning i tacoskal.',
-        price: '129:-',
-        imageUrl: ''
-      },
-  ];
+    const [food, setFood] = useState([]);
 
-  const handleOrderClick = (name: string) => {
-    
-    alert(`Du lagt till: ${name} i korgen`);
-  };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/api/menu"); // Använd /api/menu för att utnyttja proxyen
+                if (!response.ok) {
+                    throw new Error("Något gick fel");
+                }
+                const data = await response.json();
+                setFood(data);
+                console.log(data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
 
-  return (
-    <div className="matratt-container">
-      {matratterData.map((matratt) => (
-        <div className="matratt" key={matratt.id}>
-          <div className="matratt-image">
-            <img src={matratt.imageUrl} alt={matratt.name} />
-          </div>
-          <div className="matratt-details">
-            <h3>{matratt.name}</h3>
-            <p>{matratt.description}</p>
-            <p>Pris: {matratt.price} kr</p>
-            <button
-              className="order-button"
-              onClick={() => handleOrderClick(matratt.name)}
-            >
-              Lägg till
-            </button>
-          </div>
+        fetchData();
+    }, []);
+
+    const handleOrderClick = (name: string) => {
+        alert(`Du lagt till: ${name} i korgen`);
+    };
+
+    return (
+        <div className="matratt-container">
+            {food.map((matratt) => (
+                <div className="matratt" key={matratt.id}>
+                    <div className="matratt-image">
+                        <img src={matratt.img} alt={matratt.name} />
+                    </div>
+
+                    <div className="matratt-details">
+                        <h3>{matratt.name}</h3>
+                        <p className="matratt-p">{matratt.desc}</p>
+                        <div className="bottom-details">
+                            <p>Pris: {matratt.price} kr</p>
+
+                            <button
+                                className="order-button"
+                                onClick={() => handleOrderClick(matratt.name)}
+                            >
+                                Lägg till
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default Matratter;
