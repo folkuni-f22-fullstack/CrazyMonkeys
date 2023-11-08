d8b97cb45011a427996661b52117ae947eeb9512
MalinJ123 <malin--jonsson@hotmail.com>
2023-11-08 14:00:15 +0100

    startsida mobile vy princip done, ändra root font

--- src/routes/Startpage/Startpage.tsx
+++ src/routes/Startpage/Startpage.tsx
@@ -4,6 +4,8 @@ import musslor from "../../assets/startpagepic/musslor.png";
 import pizza from "../../assets/startpagepic/pizza.png";
 import sushi from "../../assets/startpagepic/sushi.png";
 import sushirulle from "../../assets/startpagepic/sushirulle.png";
+import teriyaki from "../../assets/startpagepic/teriyaki.png";
+import koriander from "../../assets/startpagepic/Korianderpesto.png";
 
 import "./startpage.css";
 
@@ -25,17 +27,43 @@ function StartPage() {
         <img className="top-pic sushi" src={sushirulle} alt="Bild på taco" />
         <img className="top-pic" src={pizza} alt="Bild på pizza" />
       </section>
+
       <main>
-        <div className="section">
+        <div className="text-container">
           <h1 className="company-title">FuNKY FUSION</h1>
           <p className="body-text">
             Fusionmat är den kreativa konsten att kombinera olika matkulturer
             och smaker för att skapa helt nya, spännande rätter.
           </p>
         </div>
-        <div className="section">
-          <img src="" alt="" />
-          <p className="body-text"></p>
+
+        <div className="text-container">
+          <img className="startmainpicture" src={koriander} alt="Bild på mat" />
+
+          <p className="body-text">
+            Korianderpesto-past - är en smakexplosion med sin friska och kryddiga
+            koriander, nötiga jordnötter och fräscha lime, vilket ger en
+            oemotståndlig smakupplevelse i varje tugga. Den kombinerar det bästa
+            av öst och väst i en pasta som är både djärv och harmonisk på samma
+            gång.
+          </p>
+        </div>
+
+        <div className="text-container">
+          <p className="companytitletext-p">
+            Fonkey Fusion är stället där du kan uppleva spännande och unika
+            matuapplevelser som förenar smaker från hela världen."
+          </p>
+        </div>
+
+        <div className="text-container">
+          <img className="startmainpicture" src={teriyaki} alt="Bild på mat" />
+
+          <p className="body-text">
+            Teriyaki Tofu Sushi Burrito - Den kombinerar den saftiga sötman från teriyaki med den fräscha
+            krispigheten av grönsaker och nori, vilket ger en spännande
+            smakupplevelse i varje tugga.
+          </p>
         </div>
       </main>
     </>
