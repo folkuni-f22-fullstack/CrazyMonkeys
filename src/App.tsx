259aa87a0bdb6ce45d1f4ee9276ea7a5c4d33e7e
Ali Al-Nasir <iraki-ali17@hotmail.com>
2023-11-09 13:46:55 +0100

    Löser konflikt

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-
 import './App.css'
+import Kundkorg from './Components/Kundkorg/Kundkorg.tsx';
 import Menu from './Components/Menu/menu'
 function App() {
 
   return (
     <>
       <Menu />
+      <Kundkorg/>
     </>
   )
 }
