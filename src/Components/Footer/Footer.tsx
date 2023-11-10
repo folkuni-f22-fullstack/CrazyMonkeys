2dc7e61a98f9f2e0c51f1d5c0a2589f659dbbab7
MalinJ123 <malin--jonsson@hotmail.com>
2023-11-10 16:22:39 +0100

    footerna är på g

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,15 +1,35 @@
 import "./footer.css";
+import { LuLogIn } from "react-icons/Lu";
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { BiSolidCopyright } from "react-icons/bi";
 
 function Footer() {
   return (
     <>
       <section className="footer-container">
-        <p>created by CrazyMonkeys</p>
+          <div className="created-by-container ">
+          
+            <p className="created-by-p"><BiSolidCopyright className="copyright-icon" />Created by CrazyMonkeys</p>
+          </div>
+
         <div className="media-container">
-            <div className="media-icons"> insta twitter fb</div>
-            <div className="phone-icon"> 054-240 240</div>
+          <div className="media icons">
+            <FaInstagram className="instagram-icon" />
+            <FaXTwitter className="twitter-icon" />
+            <FaFacebook className="facebook-icon" />
+          </div>
+
+          <div className="icon-container">
+            <BsFillTelephoneFill className="phone-icon" />
+          <p>  054-240 240</p>
+          </div>
         </div>
-        <div className="login-icon"> logga in</div>
+        <div className="created-by-container">
+          <LuLogIn className="login-icon" />
+        </div>
       </section>
     </>
   );
