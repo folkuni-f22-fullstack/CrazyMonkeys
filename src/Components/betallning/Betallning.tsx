646182b5a9ba0f2a11c24014b140ea18d594ff21
Abbe <abdullah.abou.shihab@icloud.com>
2023-11-10 10:41:59 +0100

    responsivetet

--- src/Components/betallning/Betallning.tsx
+++ src/Components/betallning/Betallning.tsx
@@ -7,120 +7,64 @@ interface CheckoutProps {
 
 const CheckoutPage: React.FC<CheckoutProps> = (props) => {
   const [paymentMethod, setPaymentMethod] = useState<string>('');
-  const [cardNumber, setCardNumber] = useState<string>('');
-  const [expiryDate, setExpiryDate] = useState<string>('');
-  const [cvv, setCvv] = useState<string>('');
-  const [swishNumber, setSwishNumber] = useState<string>('');
+  const [isButtonDisabled, setButtonDisabled] = useState<boolean>(true);
 
   const handlePaymentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPaymentMethod(event.target.value);
-  };
-
-  const handleCardNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCardNumber(event.target.value);
-  };
-
-  const handleExpiryDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setExpiryDate(event.target.value);
-  };
-
-  const handleCvvChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCvv(event.target.value);
-  };
-
-  const handleSwishNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSwishNumber(event.target.value);
+    setButtonDisabled(false); // Aktivera knappen när en betalningsmetod väljs
   };
 
   const handleProceed = () => {
-    if ((paymentMethod === 'card' && cardNumber && expiryDate && cvv) ||
-        (paymentMethod === 'swish' && swishNumber)) {
-      // Validering lyckades, gå vidare med logiken
-      console.log('Gå vidare!');
-    } else {
-      // Visa ett meddelande eller annan feedback för att informera användaren om att fylla i nödvändig information
-      console.log('Fyll i nödvändig information för att gå vidare.');
-    }
+    // Implementera logik för att gå vidare med betalningen
+    console.log('Gå vidare med betalningen för', paymentMethod);
   };
 
-  return ( 
-    <div className="checkout-container">
-      <h3>Betallningmetod</h3>
+  return (
+    <div className="outer-container">
+      <div className="checkout-container">
+        <h2>Checkout</h2>
 
-      <div className="payment-method">
-        <label>
-          <input
-            type="radio"
-            value="card"
-            checked={paymentMethod === 'card'}
-            onChange={handlePaymentChange}
-          />
-          Betala med kort
-        </label>
-      </div>
-
-      <div className="payment-method">
-        <label>
-          <input
-            type="radio"
-            value="swish"
-            checked={paymentMethod === 'swish'}
-            onChange={handlePaymentChange}
-          />
-          Betala med Swish
-        </label>
-      </div>
-
-      {paymentMethod === 'card' && (
-        <div className="payment-details">
-          <p>Fyll i kortinformation:</p>
-          <div>
-            <label htmlFor="cardNumber">Kortnummer:</label>
+        <div className="payment-method">
+          <label>
             <input
-              type="text"
-              id="cardNumber"
-              value={cardNumber}
-              onChange={handleCardNumberChange}
+              type="radio"
+              value="card"
+              checked={paymentMethod === 'card'}
+              onChange={handlePaymentChange}
             />
-          </div>
-          <div>
-            <label htmlFor="expiryDate">Utgångsdatum:</label>
-            <input
-              type="text"
-              id="expiryDate"
-              value={expiryDate}
-              onChange={handleExpiryDateChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="cvv">CVV:</label>
-            <input
-              type="text"
-              id="cvv"
-              value={cvv}
-              onChange={handleCvvChange}
-            />
-          </div>
+            Betala med kort
+          </label>
         </div>
-      )}
 
-      {paymentMethod === 'swish' && (
-        <div className="payment-details">
-          <p>Fyll i Swish-information:</p>
-          <div>
-            <label htmlFor="swishNumber">Swish-nummer:</label>
+        <div className="payment-method">
+          <label>
             <input
-              type="text"
-              id="swishNumber"
-              value={swishNumber}
-              onChange={handleSwishNumberChange}
+              type="radio"
+              value="swish"
+              checked={paymentMethod === 'swish'}
+              onChange={handlePaymentChange}
             />
-          </div>
+            Betala med Swish
+          </label>
         </div>
-      )}
 
-      <div className="proceed-button">
-        <button onClick={handleProceed}>Betala</button>
+        <div className="payment-method">
+          <label>
+            <input
+              type="radio"
+              value="cash"
+              checked={paymentMethod === 'cash'}
+              onChange={handlePaymentChange}
+            />
+            Kontant vid upphämtning
+          </label>
+        </div>
+
+        <div className="proceed-button">
+          <button onClick={handleProceed} disabled={isButtonDisabled}>
+            Gå vidare
+          </button>
+        </div>
       </div>
     </div>
   );
