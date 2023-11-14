0d6b0a2fa99c52469a31d371945903d00f4e6932
Victor (Vigge) B <vbergstrom02@gmail.com>
2023-11-14 14:30:05 +0100

    Done

--- src/routes/employee-view.tsx
+++ src/routes/employee-view.tsx
@@ -10,6 +10,14 @@ export const EmployeeView = () => {
         return selectTab === tab ? "selected-tab" : "unselected-tab";
     }
 
+    const [selectOrder, setSelectOrder] = useState({})
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        // if selectOrder is selected and has an object of an order, do this: if "skicka till kocken" button is clicked, send the order with "info till kocken" textarea text to "kockens vy"
+    } 
+
     return (
         <section className="employee-view-container">
             <header className="title-header">
@@ -27,38 +35,48 @@ export const EmployeeView = () => {
             {
                 selectTab === "untreated" && (
                     <section>
-                        <div className="order-box">
-                        <span className="material-symbols-outlined">schedule</span>
-                            <p className="order-name">Ordernummer 699</p>
-{
-    isLocked ? null : <>
-        <button className=" button-decline">Neka</button>
-        <button className=" button-edit">Ändra</button>
-    </> 
-}
-                          
-                           <details>
-                            <summary>
-                            </summary>
-                            <div className="details-about-order">
-                                <hr />
-                                <p>Maträtter: </p>
-                                <p>Tillbehör: </p>
-                                <p>Dryck(er): </p>
-                                <p>Summa: </p>
-                                <hr />
-                                <p>Kundkommentar: </p>
+                        <form onSubmit={onSubmit}>
+                            <div className="order-box">
+                            <span className="material-symbols-outlined">schedule</span>
+                                <p className="order-name">Ordernummer 699</p>
+                                {
+                                    isLocked ? <span>Skickar till kocken...</span> : <>
+                                        <button className=" button-decline">Neka</button>
+                                        <button className=" button-edit">Ändra</button>
+                                    </> 
+                                }
+                            <details onClick={() => setSelectOrder("eeeee")}>
+                                <summary>
+                                </summary>
+                                <div className="details-about-order">
+                                    <hr />
+                                    <p>Maträtter: </p>
+                                    <p>Tillbehör: </p>
+                                    <p>Dryck(er): </p>
+                                    <p>Summa: </p>
+                                    <hr />
+                                    <p>Kundkommentar: </p>
 
-                                
-<details><summary className="summary-box">Meddela kocken</summary></details>
-<details><summary className="summary-box">Info om kund</summary></details>
-                                <button className=" button-confirm" onClick={() => setIsLocked(true)}>Skicka till kocken {
-                            isLocked && <span className="material-symbols-outlined">lock</span>
-}</button>
+                                    <details><summary className="summary-box">Meddela kocken</summary>
+                                        <textarea className="msg-to-cook-textarea" placeholder="Meddelande till kocken" />
+                                    </details>
+                                    <details><summary className="summary-box">Info om kund</summary>
+                                    <p>Namn: </p>
+                                    <p>Adress: </p>
+                                    <p>Våning: </p>
+                                    <p>Portkod: </p>
+                                    <p>Mejl: </p>
+                                    <p>Telefonnummer: </p>
+                                    
+                                    </details>
+                                    <button className=" button-confirm" type="submit" onClick={() => setIsLocked(true)}>Skicka till kocken {
+                                isLocked && <span className="material-symbols-outlined">lock</span>
+    }</button>
 
+                                </div>
+                            </details>
                             </div>
-                        </details>
-                        </div>
+                        </form>
                         <hr />
                     </section>
                 )
