c94a55dcc4d64c99440d2fdbac2f8f1368303d6f
Mustafa <mustafazeinali@gmail.com>
2023-11-14 13:50:55 +0100

    it is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
-import Kundkorg from './Components/Kundkorg/Kundkorg.tsx';
+
 import Kvitto from "./Components/Kvitto/Kvitto.tsx";
 
 
-<<<<<<< HEAD
 
 function App() {
 
  return (
   <>
+
    <Kvitto/>
 
   </>
  )
-=======
-import './App.css'
-import { Delivery } from './Routes/Delivery'
-
-function App() {
-
-  return (
-    <>
-     <h1>CrazyMonekys </h1>
-     <Delivery />
-    </>
-  )
->>>>>>> feature-leverans
 }
 
 export default App
--- src/Components/Kvitto/Kvitto.tsx
+++ src/Components/Kvitto/Kvitto.tsx
@@ -1,23 +1,26 @@
 import './StyleKvitto.css'
+
 import { TbTruckDelivery } from 'react-icons/tb'
 import { AiOutlineFieldTime } from 'react-icons/ai'
 import { MdOutlinePeopleAlt } from 'react-icons/md'
-import image from './image/images.png'
+import image from './image/image1/images.png'
+import { StepsHeader } from '../StepsHeader/StepsHeader'
 const Kvitto = () => {
 
-    const resizeImage = {width:'80%' , height:'80%' , objectfit:'contain'}
+    const resizeImage = { width: '80%', height: '80%', objectfit: 'contain' }
 
     return (
 
         <main className='parent-container'>
-            <div className='container'>
-                <div className='title-line'>
+             <div className='container'>
+            <div className='title-line'>
 
 
-                    <h2 className='kvitto-title'>Orderbekräftelse</h2>
-                    <div className='message'> Tack för att du handlar på Funky Fusion</div>
-                </div>
-
+                <h2 className='kvitto-title'>Orderbekräftelse</h2>
+                <StepsHeader />
+                <div className='message'> Tack för att du handlar på Funky Fusion</div>
+            </div>
+           
                 <div className='deliver-icons'>
                     <div>
                         <span className='time'> <AiOutlineFieldTime size={35} className="timer" /> <span>15</span> <span>min</span> </span>
@@ -28,19 +31,19 @@ const Kvitto = () => {
                     <div>
                         <span className='person'><MdOutlinePeopleAlt size={35} /><span>Order nr: </span> <span>699</span></span>
                     </div>
-                   
-                   
+
+
 
                 </div>
                 <div className='delivery-pic'>
-                        <img style={resizeImage} src={image} alt="delivery pic" />
-                    </div>
+                    <img style={resizeImage} src={image} alt="delivery pic" />
+                </div>
                 <div>
-                        <p className='last-message'>
-                            Kvitto har skickats till din mejl!
-                        </p>
-                    </div>
-                <button type='button' className='confirm-btn'> kick my Ass</button>
+                    <p className='last-message'>
+                        Kvitto har skickats till din mejl!
+                    </p>
+                </div>
+                <button type='button' className='btn-grad'> Startsidan</button>
             </div>
 
         </main>
