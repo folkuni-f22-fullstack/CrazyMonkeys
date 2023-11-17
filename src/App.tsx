0fea91200fd2f7e678e827f7b3876bc7609cb8f2
Mustafa <mustafazeinali@gmail.com>
2023-11-17 16:11:42 +0100

    lagt bilderna i assets map

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 
-import Menu from './Components/Menu/menu'
-import Kundkorg from './Components/Kundkorg/Kundkorg'
-import { useContext } from "react"
-import Kvitto from "./Components/Kvitto/Kvitto.tsx";
+// import Menu from './Components/Menu/menu'
+// import Kundkorg from './Components/Kundkorg/Kundkorg'
+// import { useContext } from "react"
+// import Kvitto from "./Components/Kvitto/Kvitto.tsx";
 
-import Betallning from "./Components/betallning/Betallning.tsx"
-import { EmployeeView } from './routes/employee-view'
-import StartPage from './routes/Startpage/Startpage'
-import { Delivery } from './Routes/Delivery'
+// import Betallning from "./Components/betallning/Betallning.tsx"
+// import { EmployeeView } from './routes/employee-view'
+// import StartPage from './routes/Startpage/Startpage'
+// import { Delivery } from './Routes/Delivery'
 import AboutUs from "./Components/Om oss/Om-oss.tsx"
-import { FunkyContext } from "./ContextRoot"
+// import { FunkyContext } from "./ContextRoot"
 function App() {
 
-  const {isLoggedIn} = useContext(FunkyContext)
+  // const {isLoggedIn} = useContext(FunkyContext)
 
   return (
     <>
--- src/Components/Om oss/Om-oss.tsx
+++ src/Components/Om oss/Om-oss.tsx
@@ -1,5 +1,9 @@
 import './Om-oss.css'
-
+import image37 from '../../assets/om-oss-image/image 37.png'
+import image39 from '../../assets/om-oss-image/image 39.png'
+import image4 from '../../assets/om-oss-image/image 40.png'
+import image41 from '../../assets/om-oss-image/image 41.png'
+import image43 from '../../assets/om-oss-image/image 43.png'
 
 
 const AboutUs = () => {
@@ -8,6 +12,7 @@ const AboutUs = () => {
         <div>
             <div className='parent-text-container'>
                 <div className="text-container">
+                    <img src={image43} alt="" />
                     <p className='text-about-us'>
                         Välkomna till vår exklusiva fusionrestaurang, där smaker från hela världen möts för att skapa en unik och minnesvärd kulinarisk upplevelse. Vi är 
                     </p>
