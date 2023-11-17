d450561ee72df0ad90f7a49287f9f021c16db0a2
Victor (Vigge) B <vbergstrom02@gmail.com>
2023-11-17 15:18:49 +0100

    Removed duplicate

--- src/routes/employee-view.jsx
+++ src/routes/employee-view.jsx
@@ -1,7 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import {FunkyContext} from "../ContextRoot"
+
 import "./employeeStyle.css";
+
 import OrderKort from "../Components/anställda/OrderKort";
+
 export const EmployeeView = () => {
+    const navigate = useNavigate();
+
+    const { setIsLoggedIn, isLoggedIn } = useContext(FunkyContext);
+
     const [chartData, setChartData] = useState([]);
     const [menuNames, setMenuNames] = useState([]);
     const [orders, setOrders] = useState([]);
@@ -98,88 +107,102 @@ export const EmployeeView = () => {
         fetchData();
     }, []);
 
+    const onClickLogOut = () => {
+        setIsLoggedIn(false);
+        navigate("/");
+      };
+
     return (
-        <div className="employee-view-wrapper">
-        <section className="employee-view-container">
-            <header className="title-header">
-                <span>Du är inloggad</span>
-                <h1>Beställningar</h1>
-                <div className="title-line" />
-                <section className="tabs-section">
-                    <button
-                        className={chosenTab("untreated")}
-                        onClick={() => setSelectTab("untreated")}
-                    >
-                        Obehandlade
+        <>
+            {
+                isLoggedIn ? (
+            <div className="employee-view-wrapper">
+            <section className="employee-view-container">
+                <header className="title-header">
+                    <span>Du är inloggad</span>
+                    <button onClick={onClickLogOut} className="btn-grad">
+                        logga ut
                     </button>
-                    <button
-                        className={chosenTab("during-treatment")}
-                        onClick={() => setSelectTab("during-treatment")}
-                    >
-                        Underbehandling
-                    </button>
-                    <button className={chosenTab("done")} onClick={() => setSelectTab("done")}>
-                        Färdig
-                    </button>
-                </section>
-            </header>
+                    <h1>Beställningar</h1>
+                    <div className="title-line" />
+                    <section className="tabs-section">
+                        <button
+                            className={chosenTab("untreated")}
+                            onClick={() => setSelectTab("untreated")}
+                        >
+                            Obehandlade
+                        </button>
+                        <button
+                            className={chosenTab("during-treatment")}
+                            onClick={() => setSelectTab("during-treatment")}
+                        >
+                            Underbehandling
+                        </button>
+                        <button className={chosenTab("done")} onClick={() => setSelectTab("done")}>
+                            Färdig
+                        </button>
+                    </section>
+                </header>
 
-            {chartData.map((order) => (
-                <div key={order._id} className="order-box">
-                    <span className="material-symbols-outlined">schedule</span>
-                    <p className="order-name">Ordernummer {order.orderId}</p>
+                {chartData.map((order) => (
+                    <div key={order._id} className="order-box">
+                        <span className="material-symbols-outlined">schedule</span>
+                        <p className="order-name">Ordernummer {order.orderId}</p>
 
-                    {isLocked ? (
-                        <span>Skickar till kocken...</span>
-                    ) : (
-                        <>
-                            <button className="button-decline">Neka</button>
-                            <button className="button-edit">Ändra</button>
-                        </>
-                    )}
+                        {isLocked ? (
+                            <span>Skickar till kocken...</span>
+                        ) : (
+                            <>
+                                <button className="button-decline">Neka</button>
+                                <button className="button-edit">Ändra</button>
+                            </>
+                        )}
 
-                    <details onClick={() => setSelectOrder({ order })}>
-                        <summary></summary>
+                        <details onClick={() => setSelectOrder({ order })}>
+                            <summary></summary>
 
-                        <div className="details-about-order">
-                            <hr />
-                            {/* Render OrderKort outside the loop */}
-                            <OrderKort key={order.id} order={order} orders={orders} />
+                            <div className="details-about-order">
+                                <hr />
+                                {/* Render OrderKort outside the loop */}
+                                <OrderKort key={order.id} order={order} orders={orders} />
 
-                            <details>
-                                <summary className="summary-box">Meddela kocken</summary>
-                                <textarea
-                                    onChange={onChangeTextArea}
-                                    className="msg-to-cook-textarea"
-                                    placeholder="Meddelande till kocken"
-                                />
-                            </details>
+                                <details>
+                                    <summary className="summary-box">Meddela kocken</summary>
+                                    <textarea
+                                        onChange={onChangeTextArea}
+                                        className="msg-to-cook-textarea"
+                                        placeholder="Meddelande till kocken"
+                                    />
+                                </details>
 
-                            <details>
-                                <summary className="summary-box">Info om kund</summary>
-                                <p>Namn: {order.customerName} </p>
-                                <p>Adress: {order.adress} </p>
-                                <p>Våning: {order.floor} </p>
-                                <p>Portkod: {order.portCode} </p>
-                                <p>Mejl: {order.mail} </p>
-                                <p>Telefonnummer: {order.mobile} </p>
-                            </details>
+                                <details>
+                                    <summary className="summary-box">Info om kund</summary>
+                                    <p>Namn: {order.customerName} </p>
+                                    <p>Adress: {order.adress} </p>
+                                    <p>Våning: {order.floor} </p>
+                                    <p>Portkod: {order.portCode} </p>
+                                    <p>Mejl: {order.mail} </p>
+                                    <p>Telefonnummer: {order.mobile} </p>
+                                </details>
 
-                            <button
-                                className="button-confirm"
-                                type="submit"
-                                onClick={() => setIsLocked(true)}
-                            >
-                                Skicka till kocken{" "}
-                                {isLocked && (
-                                    <span className="material-symbols-outlined">lock</span>
-                                )}
-                            </button>
-                        </div>
-                    </details>
-                </div>
-            ))}
-        </section>
-        </div>
+                                <button
+                                    className="button-confirm"
+                                    type="submit"
+                                    onClick={() => setIsLocked(true)}
+                                >
+                                    Skicka till kocken{" "}
+                                    {isLocked && (
+                                        <span className="material-symbols-outlined">lock</span>
+                                    )}
+                                </button>
+                            </div>
+                        </details>
+                    </div>
+                ))}
+            </section>
+            </div>
+                ) : "Du är inte inloggad"
+            }
+        </>
     );
 };
