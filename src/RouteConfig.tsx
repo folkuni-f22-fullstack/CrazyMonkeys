8bc42f216cf9a6deced7fda8aab24e5df30bc498
Victor (Vigge) B <vbergstrom02@gmail.com>
2023-11-20 11:51:35 +0100

    Changed name of employee view to match the same structure as other names

--- src/RouteConfig.tsx
+++ src/RouteConfig.tsx
@@ -8,7 +8,7 @@ import Kundkorg from "./Components/Kundkorg/Kundkorg";
 import { Delivery } from "./Routes/Delivery";
 import CheckoutPage from "./Components/Betallning/Betallning.tsx";
 import Kvitto from "./Components/Kvitto/Kvitto.tsx";
-import { EmployeeView } from "./Routes/employee-view.jsx";
+import { EmployeeView } from "./Routes/EmployeeView.jsx";
 
 const router = createBrowserRouter([
   {
