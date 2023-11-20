ebd0631d9fe8f523df4e729c3ecaaaeb0311a05f
Mustafa <mustafazeinali@gmail.com>
2023-11-20 23:54:21 +0100

    i am on the way in desgin

--- src/Components/Om oss/Om-oss.tsx
+++ src/Components/Om oss/Om-oss.tsx
@@ -1,21 +1,55 @@
 import './Om-oss.css'
-import image37 from '../../assets/om-oss-image/image 37.png'
+import wok from '../../assets/om-oss-image/wok.png'
 import image39 from '../../assets/om-oss-image/image 39.png'
-import image4 from '../../assets/om-oss-image/image 40.png'
+import pasta from '../../assets/om-oss-image/pasta.png'
 import image41 from '../../assets/om-oss-image/image 41.png'
-import image43 from '../../assets/om-oss-image/image 43.png'
+import titlePic from '../../assets/om-oss-image/titlePic.png'
 
 
 const AboutUs = () => {
 
+    const resizePic = { minWidth: '100%' }
     return (
         <div>
-            <div className='parent-text-container'>
-                <div className="text-container">
-                    <img src={image43} alt="" />
-                    <p className='text-about-us'>
-                        Välkomna till vår exklusiva fusionrestaurang, där smaker från hela världen möts för att skapa en unik och minnesvärd kulinarisk upplevelse. Vi är 
-                    </p>
+            <div className='parent-about-container'>
+                <div className="about-container">
+                    <section className='title-pic'>
+                        <img className='first-img' style={resizePic} src={titlePic} alt="bild på mat" />
+                        <p className='mobile-text'>
+                            om oss på funky fusion
+                        </p>
+                        <p className='text-title-us'>
+                            Välkomna till vår exklusiva fusionrestaurang, där smaker från hela världen möts för att skapa en unik och minnesvärd kulinarisk upplevelse. Vi är
+                        </p>
+                        </section>
+                        <section className='middle-page'>
+                        <img src={wok} alt="bild på mat" className='wok-img' />
+                        <img src={image41} alt="bild på mat" className='pasta-img' />
+                            <div className='middle-contain'>
+                            
+                                <div className='funky-about-text'>
+                                    <h1 className='whyfunky'> Varför välja Funkey Fusion </h1>
+                                    <p className='middle-text'>
+                                        Välkomna till vår exklusiva fusionrestaurang, där smaker från hela världen möts för att skapa en unik och minnesvärd kulinarisk upplevelse. Vi är stolta över att erbjuda en atmosfär fylld av elegans och en meny som är lika mångsidig som den är läcker.
+
+                                        Vårt passionerade köksteam sätter tonen för en gastronomisk resa där traditionella rätter från olika kulturer kombineras på nyskapande sätt. Varje rätt är en konstnärlig fusion av smaker och ingredienser, noggrant utvalda för att skapa harmoni på tallriken.
+                                        Vi strävar efter att erbjuda våra gäster en kulinarisk resa där varje tugga är en smakupplevelse i världsklass.
+
+
+
+                                    </p>
+                                </div>
+                            </div>
+
+
+                           
+                        </section>
+                    
+                    <div>
+
+                    </div>
+
+
                 </div>
             </div>
 
