f44a38b32c59533ec6892f72a1ce729b851331f9
Mustafa <mustafazeinali@gmail.com>
2023-11-21 11:56:02 +0100

    ska göra kaos med den design

--- src/Components/Om oss/Om-oss.tsx
+++ src/Components/Om oss/Om-oss.tsx
@@ -1,8 +1,8 @@
 import './Om-oss.css'
 import wok from '../../assets/om-oss-image/wok.png'
 import image39 from '../../assets/om-oss-image/image 39.png'
+import seafood from '../../assets/om-oss-image/seafood.png'
 import pasta from '../../assets/om-oss-image/pasta.png'
-import image41 from '../../assets/om-oss-image/image 41.png'
 import titlePic from '../../assets/om-oss-image/titlePic.png'
 
 
@@ -21,30 +21,33 @@ const AboutUs = () => {
                         <p className='text-title-us'>
                             Välkomna till vår exklusiva fusionrestaurang, där smaker från hela världen möts för att skapa en unik och minnesvärd kulinarisk upplevelse. Vi är
                         </p>
-                        </section>
-                        <section className='middle-page'>
-                        <img src={wok} alt="bild på mat" className='wok-img' />
-                        <img src={image41} alt="bild på mat" className='pasta-img' />
-                            <div className='middle-contain'>
-                            
-                                <div className='funky-about-text'>
-                                    <h1 className='whyfunky'> Varför välja Funkey Fusion </h1>
-                                    <p className='middle-text'>
-                                        Välkomna till vår exklusiva fusionrestaurang, där smaker från hela världen möts för att skapa en unik och minnesvärd kulinarisk upplevelse. Vi är stolta över att erbjuda en atmosfär fylld av elegans och en meny som är lika mångsidig som den är läcker.
+                    </section>
+                    <section className='middle-page'>
+                        <div className='middle-img'>
+                            <img src={wok} alt="bild på mat" className='wok-img' />
+                            <img src={pasta} alt="bild på mat" className='pasta-img' />
+                        </div>
 
-                                        Vårt passionerade köksteam sätter tonen för en gastronomisk resa där traditionella rätter från olika kulturer kombineras på nyskapande sätt. Varje rätt är en konstnärlig fusion av smaker och ingredienser, noggrant utvalda för att skapa harmoni på tallriken.
-                                        Vi strävar efter att erbjuda våra gäster en kulinarisk resa där varje tugga är en smakupplevelse i världsklass.
+                        <div className='middle-contain'>
+
+                            <div className='funky-about-text'>
+                                <h1 className='whyfunky'> Varför välja Funkey Fusion </h1>
+                                <p className='middle-text'>
+                                    Välkomna till vår exklusiva fusionrestaurang, där smaker från hela världen möts för att skapa en unik och minnesvärd kulinarisk upplevelse. Vi är stolta över att erbjuda en atmosfär fylld av elegans och en meny som är lika mångsidig som den är läcker.
+
+                                    Vårt passionerade köksteam sätter tonen för en gastronomisk resa där traditionella rätter från olika kulturer kombineras på nyskapande sätt. Varje rätt är en konstnärlig fusion av smaker och ingredienser, noggrant utvalda för att skapa harmoni på tallriken.
+                                    Vi strävar efter att erbjuda våra gäster en kulinarisk resa där varje tugga är en smakupplevelse i världsklass.
 
 
 
-                                    </p>
-                                </div>
+                                </p>
                             </div>
+                        </div>
 
 
-                           
-                        </section>
-                    
+
+                    </section>
+
                     <div>
 
                     </div>
