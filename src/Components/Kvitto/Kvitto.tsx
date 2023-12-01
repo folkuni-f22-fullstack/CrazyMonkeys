8f436e2f88ace0befd02565595d10efdd8d812d3
Victor (Vigge) B <vbergstrom02@gmail.com>
2023-12-01 10:35:50 +0100

    Done

--- src/Components/Kvitto/Kvitto.tsx
+++ src/Components/Kvitto/Kvitto.tsx
@@ -7,14 +7,13 @@ import image from './image/image1/images.png';
 import { StepsHeader } from '../StepsHeader/StepsHeader';
 import './StyleKvitto.css';
 import { FunkyContext } from '../../ContextRoot';
-// import { FunkyContext } from '../../ContextRoot';
 
 interface KvittoProps {
   orderNumber: number;
 }
 
 const Kvitto: React.FC<KvittoProps> = ({ orderNumber }) => {
-    const {chosenDeliveryOption, recipeId } = useContext(FunkyContext)
+    const {chosenDeliveryOption, recipeId, order } = useContext(FunkyContext)
 
   // const {chosenDeliveryOption} = useContext(FunkyContext)
   const resizeImageStyle = { width: '80%', height: '80%', objectFit: 'contain' };
@@ -30,65 +29,66 @@ const Kvitto: React.FC<KvittoProps> = ({ orderNumber }) => {
 
   const isFoodReady = getRandomBoolean();
   const deliveryTime = getRandomDeliveryTime();
-
+  console.log(order);
   return (
     <main className='parent-container'>
       <div className='container'>
-        <div className='title-line'>
           <h2 className='kvitto-title'>Orderbekräftelse</h2>
-          <span className='stephead'>
             <StepsHeader />
-          </span>
-          <div className='message'>Tack för att du handlar på Funky Fusion</div>
-        </div>
-
-        <div className='deliver-icons'>
-            {!chosenDeliveryOption ? 
-          <div>
-            <span className='time'>
-              <AiOutlineFieldTime size={35} className="timer" />
-              <span>Order Time: {isFoodReady} min</span>
-            </span>
-          </div>
-           :
-          <div>
-            <span className='truck'>
-              <TbTruckDelivery size={35} />
-              <span>Leverans: {deliveryTime} min</span>
-            </span>
-          </div> 
-        }
-
-          <div>
-            <span className='person'>
-              <MdOutlinePeopleAlt size={35} />
-              <span>Order nr: {recipeId}</span>
-            </span>
-          </div>
-        </div>
-
-        <div className='delivery-pic'>
-          <img style={resizeImageStyle} src={image} alt="delivery pic" />
-        </div>
-
-        <div>
-          <p className='last-message'>Kvitto har skickats till din mejl!</p>
-        </div>
-
-        <Link to="/" className="btn-grad">Tillbaka till start</Link>
+          {
+            order.length > 0 ? (
+              <>
+                 <div className='message'>Tack för att du handlar på Funky Fusion</div>
+              <div className='deliver-icons'>
+                  {!chosenDeliveryOption ? 
+                <div>
+                  <span className='time'>
+                    <AiOutlineFieldTime size={35} className="timer" />
+                    <span>Order tid: {isFoodReady} min</span>
+                  </span>
+                </div>
+                 :
+                <div>
+                  <span className='truck'>
+                    <TbTruckDelivery size={35} />
+                    <span>Leverans: {deliveryTime} min</span>
+                  </span>
+                </div> 
+              }
+      
+                <div>
+                  <span className='person'>
+                    <MdOutlinePeopleAlt size={35} />
+                    <span>Order nr: {recipeId}</span>
+                  </span>
+                </div>
+              </div>
+      
+              <div className='delivery-pic'>
+                <img style={resizeImageStyle} src={image} alt="delivery pic" />
+              </div>
+      
+              <div>
+                <p className='last-message'>Kvitto har skickats till din mejl!</p>
+              </div>
+      
+              <Link to="/" className="btn-grad">Tillbaka till start</Link>
+              </>
+             
+            ) : (
+              <>
+                <div className="receipt-center">
+                  <p>Du måste ha slutfört en beställning för att få en orderbekräftelse!</p>
+                </div>
+                <Link to="/menu" className="btn-grad">Tillbaka till menyn</Link>
+              </>
+            )
+          }
+ 
       </div>
     </main>
   );
 };
 
 export default Kvitto;
-// /div>
-
-//         <Link to="/" className="btn-grad">Tillbaka till start</Link>
-//       </div>
-//     </main>
-//   );
-// };
-
-// export default Kvitto;
 
