1e101d1b8b18ab3943be86657257b9408b84b29a
Ali Al-Nasir <iraki-ali17@hotmail.com>
2023-12-05 14:55:11 +0100

    Last update

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import menuRoute from "./routes/menu.js"
+import menuRoute from "./routes/menu.js";
 import messagesRoute from "./routes/message.js";
 import orderRoute from "./routes/orders.js";
 import authRoute from "./routes/users.js";
@@ -12,7 +12,11 @@ dotenv.config();
 const app = express();
 
 mongoose
-    .connect(process.env.MONGO_URL, {dbName: `FunkyFusion` ,useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(process.env.MONGO_URL, {
+        dbName: `FunkyFusion`,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => {
         console.log("Connected to MongoDB");
     })
@@ -21,16 +25,25 @@ mongoose
         console.error("Could not connect to MongoDB:", error);
     });
 
-      //Middleware
+    const whereWeAre = dirname(fileURLToPath(import.meta.url));
+    const dist = join(whereWeAre, "./dist");
+    app.use(express.static(dist));
+    
+    app.options("*", (req, res) => {
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
+        res.send();
+    });
+    
+    //Middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use("/api/menu", menuRoute)
-app.use("/api/messages", messagesRoute)
-app.use("/api/orders", orderRoute)
-app.use("/api/auth", authRoute)
-
+app.use("/api/menu", menuRoute);
+app.use("/api/messages", messagesRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(3114, () => {
     console.log("Backend server is running");
