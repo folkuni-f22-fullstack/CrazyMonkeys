b7bb574813dda4300a708817c0be3dd1455abd07
Ali Al-Nasir <iraki-ali17@hotmail.com>
2023-12-05 14:39:23 +0100

    .

--- src/routes/Chefsview/Chefsview.jsx
+++ src/routes/Chefsview/Chefsview.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useContext } from "react";
 import OrderKort from "../../Components/anställda/OrderKort";
-import "../../routes/Chefsview/Chefsview.css";
+import "../../Routes/Chefsview/chefsview.css";
 import UnderTreatmentOrder from "../../Components/Orders/underTreatmentOrder";
 import { FunkyContext } from "../../ContextRoot";
 import { handleLogout } from "../../Components/Login/loginFetch";
