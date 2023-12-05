e1b8c3474e6c2e3b2490849bc91ec9f0a40d8f81
Ali Al-Nasir <iraki-ali17@hotmail.com>
2023-12-05 11:46:14 +0100

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        rollupOptions: {
+          external: ['react-icons/Lu'],
+        },
+      },
   plugins: [react()],
   server: {
     proxy: {
